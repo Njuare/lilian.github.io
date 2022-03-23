12378a4787158054d333bc73f2c2f0abdf0516b8
Njuare <ewalilian0@gmail.com>
2022-03-23 12:47:47 +0100

    lovely cats

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+//challenge 2: Cat Generator.
+
+function generatecat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "http://thecatapi.com/api/images/get?format=src&type=gif";
+    div.appendChild(image);
+   
+}
+
+
+
